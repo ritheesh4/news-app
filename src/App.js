f18790a8786194cb24feef60db81129604a1d297
Ritheesh <ritheesh.4@gmail.com>
2020-10-17 10:17:56 +0530

    redux componentes added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/hompepage.component';
 import News from './pages/news/news.component';
 
 class App extends React.Component {
-  render() {
 
+
+  componentDidMount() {
+    const state = {
+      newsList: []
+    }
+    fetch('http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=69cf4a8328d54e23921a8be3e595f01b')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ newsList: data })
+      });
+    console.log(state)
+
+  }
+  render() {
     return (
       <div>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route path='/news' component={News} />
-          </Switch>
-        </BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/news' component={News} />
+        </Switch>
       </div>
     );
   }
---
+++ src/redux/home/home.reducer.js
@@ -0,0 +1,19 @@
+import { HomeActionTypes } from './home.types';
+
+const INITIAL_STATE = {
+    newsList: null
+}
+
+const homeReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case HomeActionTypes.SET_NEWS:
+            return {
+                ...state,
+                newsList: action.payload
+            }
+        default:
+            return state;
+    }
+}
+
+export default homeReducer;
