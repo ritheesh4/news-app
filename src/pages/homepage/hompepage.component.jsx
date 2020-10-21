730406091f4ada7fca426baedddc06d951b32484
Ritheesh <ritheesh.4@gmail.com>
2020-10-21 17:34:42 +0530

    footer added

--- src/pages/homepage/hompepage.component.jsx
+++ src/pages/homepage/hompepage.component.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import NavigationBar from "../../components/nav-bar/nav-bar.component";
 import CardsContainer from "../../components/cards-container/cards-container.component";
+import Footer from '../../components/footer/footer.component';
+
 
 import "./homepage.styles.scss";
 const HomePage = () => (
   <div className="homepage">
     <NavigationBar/>
     <CardsContainer />
+    <Footer/>
   </div>
 );
 
